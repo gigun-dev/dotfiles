e2541416a94da4ecd12d983d3c1e341453796124
gigun <117321963+gigun-dev@users.noreply.github.com>
2026-04-11 13:29:04 +0900

    feat: Git に ghq.user を設定

--- nix/modules/home/programs/git/default.nix
+++ nix/modules/home/programs/git/default.nix
@@ -13,6 +13,7 @@
         name = "gigun";
         email = "117321963+gigun-dev@users.noreply.github.com";
       };
+      ghq.user = "gigun-dev";
       init.defaultBranch = "main";
       pull.rebase = true;
       push.autoSetupRemote = true;
